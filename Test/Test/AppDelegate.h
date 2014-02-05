26a485b3560f72ccb9247288268dc01a572cc53a
Juan Sierra <contact.juan.sierra@gmail.com>
2014-02-05 19:49:00 +0100

    initial commit

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Juan Sierra on 05/02/2014.
+//  Copyright (c) 2014 Juan Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Juan Sierra on 05/02/2014.
+//  Copyright (c) 2014 Juan Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
